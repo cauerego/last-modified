b36f7ac796e437c22d614800ecb03db2150b0ffa
Michael Weber <msweber@gmail.com>
2014-09-06 16:00:28 +1000

    Initial commit.

---
+++ main.js
@@ -0,0 +1,34 @@
+var el;
+
+function getLastModifiedText() {
+	var lastModified = document.lastModified;
+	if (typeof lastModified == "string" && lastModified.length > 0) {
+		return "<strong>Last Modified:</strong><br>" + lastModified + "<br><em>(timezone is UTC)</em>"; 
+	} else {
+		return "Could not determine a last modified date.";
+	}
+}
+
+function showElement() {
+	if (!el) {
+		var lastModified = document.lastModified,
+		el = document.createElement("div");
+		el.className = "last-modified visible";
+		el.innerHTML = getLastModifiedText();
+		el.onclick = function(e) {
+			e.preventDefault();
+			hideElement(e.target);
+		}
+		document.body.appendChild(el);
+	} else {
+		el.className = "last-modified visible";
+	}
+}
+
+function hideElement(el) {
+	el.className = "last-modified hidden";
+}
+
+window.onload = function() {
+	showElement();
+}
